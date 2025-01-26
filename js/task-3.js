de7fb4b2da1bac066c73026558d1c4c732ea52b9
PSV <psv0782@gmail.com>
2025-01-26 16:11:41 +0000

    done all 4 tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,12 @@
-function getElementWidth(content, padding, border) {
-  const contentNumber = Number.parseFloat(content);
-  const paddingNumber = Number.parseFloat(padding);
-  const borderNumber = Number.parseFloat(border);
-  return Math.round(contentNumber + paddingNumber*2 + borderNumber*2);
+function checkForSpam(message) {
+  let modifiedMessageForSearch = message.toLowerCase();
+  return (modifiedMessageForSearch.includes('spam') || modifiedMessageForSearch.includes('sale')) ? true : false;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
